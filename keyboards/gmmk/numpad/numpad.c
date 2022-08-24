88b1060a2c3566909dc36f6ffb7e1dc08058a6f4
jloliverio <jorge.oliverio@gmail.com>
2022-08-24 17:27:43 -0300

    -Index to LEDs drive mapping table finished

--- keyboards/gmmk/numpad/numpad.c
+++ keyboards/gmmk/numpad/numpad.c
@@ -15,7 +15,7 @@
  */
 
 #include "numpad.h"
-
+#define RGB_MATRIX_ENABLE
 #ifdef RGB_MATRIX_ENABLE
 
 const aw_led g_aw_leds[DRIVER_LED_TOTAL] = {
@@ -23,44 +23,42 @@ const aw_led g_aw_leds[DRIVER_LED_TOTAL] = {
  * and 0xD7 inclusive.
  * See drivers/awinic/aw20216.h for the mapping between register offsets and
  * driver pin locations.
- *    driver
- *    |  R location
- *    |  |        G location
- *    |  |        |        B location
- *    |  |        |        | */
-    {0, CS1_SW1,   CS2_SW1,   CS3_SW1  },  //  0  LED0
-    {0, CS1_SW2,   CS2_SW2,   CS3_SW2  },  //  1  NUM
-    {0, CS1_SW3,   CS2_SW3,   CS3_SW3  },  //  2  /
-    {0, CS1_SW4,   CS2_SW4,   CS3_SW4  },  //  3  *
-    {0, CS1_SW5,   CS2_SW5,   CS3_SW5  },  //  4  -
-    {0, CS1_SW6,   CS2_SW6,   CS3_SW6  },  //  5  LED5
-    {0, CS4_SW1,   CS5_SW1,   CS6_SW1  },  //  6  LED6
-    {0, CS4_SW2,   CS5_SW2,   CS6_SW2  },  //  7  7
-    {0, CS4_SW3,   CS5_SW3,   CS6_SW3  },  //  8  8
-    {0, CS4_SW4,   CS5_SW4,   CS6_SW4  },  //  9  9
-    {0, CS4_SW5,   CS5_SW5,   CS6_SW5  },  //  10 +
-    {0, CS4_SW6,   CS5_SW6,   CS6_SW6  },  //  11 LED11
-    {0, CS7_SW1,   CS8_SW1,   CS9_SW1  },  //  12 LED12
-    {0, CS7_SW2,   CS8_SW2,   CS9_SW2  },  //  13 LED13
-    {0, CS7_SW3,   CS8_SW3,   CS9_SW3  },  //  14 LED14
-    {0, CS7_SW4,   CS8_SW4,   CS9_SW4  },  //  15 LED15
-    {0, CS7_SW5,   CS8_SW5,   CS9_SW5  },  //  16 LED16
-    {0, CS7_SW6,   CS8_SW6,   CS9_SW6  },  //  17 LED17
-    {0, CS10_SW1,  CS11_SW1,  CS12_SW1 },  //  18 LED18
-    {0, CS10_SW2,  CS11_SW2,  CS12_SW2 },  //  19 4
-    {0, CS10_SW3,  CS11_SW3,  CS12_SW3 },  //  20 5
-    {0, CS10_SW4,  CS11_SW4,  CS12_SW4 },  //  21 6
-    {0, CS10_SW5,  CS11_SW5,  CS12_SW5 },  //  22 CALC
-    {0, CS10_SW6,  CS11_SW6,  CS12_SW6 },  //  23 LED23
-    {0, CS13_SW1,  CS14_SW1,  CS15_SW1 },  //  24 LED24
-    {0, CS13_SW2,  CS14_SW2,  CS15_SW2 },  //  25 1
-    {0, CS13_SW3,  CS14_SW3,  CS15_SW3 },  //  26 2
-    {0, CS13_SW4,  CS14_SW4,  CS15_SW4 },  //  27 3
-    {0, CS13_SW5,  CS14_SW5,  CS15_SW5 },  //  28 ENTER
-    {0, CS13_SW6,  CS14_SW6,  CS15_SW6 },  //  29 LED29
-    {0, CS16_SW1,  CS17_SW1,  CS18_SW1 },  //  30 LED30
-    {0, CS16_SW2,  CS17_SW2,  CS18_SW2 },  //  31 LED31
-    {0, CS16_SW3,  CS17_SW3,  CS18_SW3 }   //  32 LED32
+ *   driver
+ *   |  R location
+ *   |  |         G location
+ *   |  |         |         B location
+ *   |  |         |         | */
+    {0, CS4_SW1,  CS5_SW1,  CS6_SW1  },  //  0  NUM
+    {0, CS4_SW2,  CS5_SW2,  CS6_SW2  },  //  1  /
+    {0, CS7_SW1,  CS8_SW1,  CS9_SW1  },  //  2  *
+    {0, CS7_SW2,  CS8_SW2,  CS9_SW2  },  //  3  -
+    {0, CS4_SW3,  CS5_SW3,  CS6_SW3  },  //  4  7
+    {0, CS4_SW4,  CS5_SW4,  CS6_SW4  },  //  5  8
+    {0, CS7_SW3,  CS8_SW3,  CS9_SW3  },  //  6  9
+    {0, CS7_SW4,  CS8_SW4,  CS9_SW4  },  //  7  +
+    {0, CS4_SW5,  CS5_SW5,  CS6_SW5  },  //  8  4
+    {0, CS4_SW6,  CS5_SW6,  CS6_SW6  },  //  9  5
+    {0, CS7_SW5,  CS8_SW5,  CS9_SW5  },  //  10 6
+    {0, CS4_SW7,  CS5_SW7,  CS6_SW7  },  //  11 1
+    {0, CS4_SW8,  CS5_SW8,  CS6_SW8  },  //  12 2
+    {0, CS7_SW7,  CS8_SW7,  CS9_SW7  },  //  13 3
+    {0, CS7_SW8,  CS8_SW8,  CS9_SW8  },  //  14 ENTER
+    {0, CS4_SW9,  CS5_SW9,  CS6_SW9  },  //  15 0
+    {0, CS7_SW9,  CS8_SW9,  CS9_SW9  },  //  16 .
+    {0, CS1_SW1,  CS2_SW1,  CS3_SW1  },  //  17 LED18
+    {0, CS1_SW2,  CS2_SW2,  CS3_SW2  },  //  18 LED19
+    {0, CS1_SW3,  CS2_SW3,  CS3_SW3  },  //  19 LED20
+    {0, CS1_SW4,  CS2_SW4,  CS3_SW4  },  //  20 LED21
+    {0, CS1_SW5,  CS2_SW5,  CS3_SW5  },  //  21 LED22
+    {0, CS1_SW6,  CS2_SW6,  CS3_SW6  },  //  22 LED23
+    {0, CS1_SW7,  CS2_SW7,  CS3_SW7  },  //  23 LED24
+    {0, CS10_SW1, CS11_SW1, CS12_SW1 },  //  24 LED27
+    {0, CS10_SW2, CS11_SW2, CS12_SW2 },  //  25 LED28
+    {0, CS10_SW3, CS11_SW3, CS12_SW3 },  //  26 LED29
+    {0, CS10_SW4, CS11_SW4, CS12_SW4 },  //  27 LED30
+    {0, CS10_SW5, CS11_SW5, CS12_SW5 },  //  28 LED31
+    {0, CS10_SW6, CS11_SW6, CS12_SW6 },  //  29 LED32
+    {0, CS10_SW7, CS11_SW7, CS12_SW7 },  //  30 LED33
 };
 
 #undef ___
