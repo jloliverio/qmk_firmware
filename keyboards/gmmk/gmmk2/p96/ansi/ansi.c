93ffb331d53d7fe5771feeece8588113942e4d4d
jloliverio <jorge.oliverio@gmail.com>
2022-08-26 14:25:39 -0300

    -

--- keyboards/gmmk/gmmk2/p96/ansi/ansi.c
+++ keyboards/gmmk/gmmk2/p96/ansi/ansi.c
@@ -25,13 +25,44 @@ const aw_led g_aw_leds[DRIVER_LED_TOTAL] = {
  *   |     |          G location
  *   |     |          |          B location
  *   |     |          |          | */
-    {0, CS1_SW1,   CS2_SW1,   CS3_SW1},   //*   0, k00, Esc
-    {0, CS1_SW2,   CS2_SW2,   CS3_SW2},   //*   1, k10, F1
-    {0, CS1_SW3,   CS2_SW3,   CS3_SW3},   //*   2, k20, F2
-    {0, CS1_SW4,   CS2_SW4,   CS3_SW4},   //*   3, k30, F3
-    {0, CS1_SW5,   CS2_SW5,   CS3_SW5},   //*   4, k40, F4
-    {0, CS1_SW6,   CS2_SW6,   CS3_SW6},   //*   5, k50, F5
-    {0, CS1_SW7,   CS2_SW7,   CS3_SW7},   //*   6, k60, F6
+    {0, CS4_SW1,  CS5_SW1,  CS6_SW1  },  //  0  NUM
+    {0, CS4_SW2,  CS5_SW2,  CS6_SW2  },  //  1  /
+    {0, CS7_SW1,  CS8_SW1,  CS9_SW1  },  //  2  *
+    {0, CS7_SW2,  CS8_SW2,  CS9_SW2  },  //  3  -
+    {0, CS4_SW3,  CS5_SW3,  CS6_SW3  },  //  4  7
+    {0, CS4_SW4,  CS5_SW4,  CS6_SW4  },  //  5  8
+    {0, CS7_SW3,  CS8_SW3,  CS9_SW3  },  //  6  9
+    {0, CS7_SW4,  CS8_SW4,  CS9_SW4  },  //  7  +
+    {0, CS4_SW5,  CS5_SW5,  CS6_SW5  },  //  8  4
+    {0, CS4_SW6,  CS5_SW6,  CS6_SW6  },  //  9  5
+    {0, CS7_SW5,  CS8_SW5,  CS9_SW5  },  //  10 6
+    {0, CS4_SW7,  CS5_SW7,  CS6_SW7  },  //  11 1
+    {0, CS4_SW8,  CS5_SW8,  CS6_SW8  },  //  12 2
+    {0, CS7_SW7,  CS8_SW7,  CS9_SW7  },  //  13 3
+    {0, CS7_SW8,  CS8_SW8,  CS9_SW8  },  //  14 ENTER
+    {0, CS4_SW9,  CS5_SW9,  CS6_SW9  },  //  15 0
+    {0, CS7_SW9,  CS8_SW9,  CS9_SW9  },  //  16 .
+    {0, CS1_SW1,  CS2_SW1,  CS3_SW1  },  //  17 LED18
+    {0, CS1_SW2,  CS2_SW2,  CS3_SW2  },  //  18 LED19
+    {0, CS1_SW3,  CS2_SW3,  CS3_SW3  },  //  19 LED20
+    {0, CS1_SW4,  CS2_SW4,  CS3_SW4  },  //  20 LED21
+    {0, CS1_SW5,  CS2_SW5,  CS3_SW5  },  //  21 LED22
+    {0, CS1_SW6,  CS2_SW6,  CS3_SW6  },  //  22 LED23
+    {0, CS1_SW7,  CS2_SW7,  CS3_SW7  },  //  23 LED24
+    {0, CS10_SW1, CS11_SW1, CS12_SW1 },  //  24 LED27
+    {0, CS10_SW2, CS11_SW2, CS12_SW2 },  //  25 LED28
+    {0, CS10_SW3, CS11_SW3, CS12_SW3 },  //  26 LED29
+    {0, CS10_SW4, CS11_SW4, CS12_SW4 },  //  27 LED30
+    {0, CS10_SW5, CS11_SW5, CS12_SW5 },  //  28 LED31
+    {0, CS10_SW6, CS11_SW6, CS12_SW6 },  //  29 LED32
+    {0, CS10_SW7, CS11_SW7, CS12_SW7 },  //  30 LED33
+    //{0, CS1_SW1,   CS2_SW1,   CS3_SW1},   //*   0, k00, Esc
+    //{0, CS1_SW2,   CS2_SW2,   CS3_SW2},   //*   1, k10, F1
+    //{0, CS1_SW3,   CS2_SW3,   CS3_SW3},   //*   2, k20, F2
+    //{0, CS1_SW4,   CS2_SW4,   CS3_SW4},   //*   3, k30, F3
+    //{0, CS1_SW5,   CS2_SW5,   CS3_SW5},   //*   4, k40, F4
+    //{0, CS1_SW6,   CS2_SW6,   CS3_SW6},   //*   5, k50, F5
+    //{0, CS1_SW7,   CS2_SW7,   CS3_SW7},   //*   6, k60, F6
     {0, CS1_SW8,   CS2_SW8,   CS3_SW8},   //   7, k70, F7
     {0, CS1_SW9,   CS2_SW9,   CS3_SW9},   //   8, k80, F8
     {0, CS1_SW10,  CS2_SW10,  CS3_SW10},  //   9, k90, F9
@@ -44,15 +75,15 @@ const aw_led g_aw_leds[DRIVER_LED_TOTAL] = {
     {1, CS4_SW4,   CS5_SW4,   CS6_SW4},   //  16, k26, Page Up
     {1, CS4_SW7,   CS5_SW7,   CS6_SW7},   //  17, k36, Page Down
 
-    {0, CS4_SW1,   CS5_SW1,   CS6_SW1},   //*  18, k01, `
-    {0, CS4_SW2,   CS5_SW2,   CS6_SW2},   //*  19, k11, 1
-    {0, CS4_SW3,   CS5_SW3,   CS6_SW3},   //*  20, k21, 2
-    {0, CS4_SW4,   CS5_SW4,   CS6_SW4},   //*  21, k31, 3
-    {0, CS4_SW5,   CS5_SW5,   CS6_SW5},   //*  22, k41, 4
-    {0, CS4_SW6,   CS5_SW6,   CS6_SW6},   //*  23, k51, 5
-    {0, CS4_SW7,   CS5_SW7,   CS6_SW7},   //*  24, k61, 6
-    {0, CS4_SW8,   CS5_SW8,   CS6_SW8},   //*  25, k71, 7
-    {0, CS4_SW9,   CS5_SW9,   CS6_SW9},   //*  26, k81, 8
+    //{0, CS4_SW1,   CS5_SW1,   CS6_SW1},   //*  18, k01, `
+    //{0, CS4_SW2,   CS5_SW2,   CS6_SW2},   //*  19, k11, 1
+    //{0, CS4_SW3,   CS5_SW3,   CS6_SW3},   //*  20, k21, 2
+    //{0, CS4_SW4,   CS5_SW4,   CS6_SW4},   //*  21, k31, 3
+    //{0, CS4_SW5,   CS5_SW5,   CS6_SW5},   //*  22, k41, 4
+    //{0, CS4_SW6,   CS5_SW6,   CS6_SW6},   //*  23, k51, 5
+    //{0, CS4_SW7,   CS5_SW7,   CS6_SW7},   //*  24, k61, 6
+    //{0, CS4_SW8,   CS5_SW8,   CS6_SW8},   //*  25, k71, 7
+    //{0, CS4_SW9,   CS5_SW9,   CS6_SW9},   //*  26, k81, 8
     {0, CS4_SW10,  CS5_SW10,  CS6_SW10},  //  27, k91, 9
     {0, CS4_SW11,  CS5_SW11,  CS6_SW11},  //  28, ka1, 0
     {0, CS4_SW12,  CS5_SW12,  CS6_SW12},  //  29, kb1, -
@@ -63,15 +94,15 @@ const aw_led g_aw_leds[DRIVER_LED_TOTAL] = {
     {1, CS7_SW3,   CS8_SW3,   CS9_SW3},   //  34, k66, Num *
     {1, CS7_SW4,   CS8_SW4,   CS9_SW4},   //  35, k76, Num -
 
-    {0, CS7_SW1,   CS8_SW1,   CS9_SW1},   //*  36, k02, Tab
-    {0, CS7_SW2,   CS8_SW2,   CS9_SW2},   //*  37, k12, Q
-    {0, CS7_SW3,   CS8_SW3,   CS9_SW3},   //*  38, k22, W
-    {0, CS7_SW4,   CS8_SW4,   CS9_SW4},   //*  39, k32, E
-    {0, CS7_SW5,   CS8_SW5,   CS9_SW5},   //*  40, k42, R
+    //{0, CS7_SW1,   CS8_SW1,   CS9_SW1},   //*  36, k02, Tab
+    //{0, CS7_SW2,   CS8_SW2,   CS9_SW2},   //*  37, k12, Q
+    //{0, CS7_SW3,   CS8_SW3,   CS9_SW3},   //*  38, k22, W
+    //{0, CS7_SW4,   CS8_SW4,   CS9_SW4},   //*  39, k32, E
+    //{0, CS7_SW5,   CS8_SW5,   CS9_SW5},   //*  40, k42, R
     {0, CS7_SW6,   CS8_SW6,   CS9_SW6},   //  41, k52, T
-    {0, CS7_SW7,   CS8_SW7,   CS9_SW7},   //*  42, k62, Y
-    {0, CS7_SW8,   CS8_SW8,   CS9_SW8},   //*  43, k72, U
-    {0, CS7_SW9,   CS8_SW9,   CS9_SW9},   //*  44, k82, I
+    //{0, CS7_SW7,   CS8_SW7,   CS9_SW7},   //*  42, k62, Y
+    //{0, CS7_SW8,   CS8_SW8,   CS9_SW8},   //*  43, k72, U
+    //{0, CS7_SW9,   CS8_SW9,   CS9_SW9},   //*  44, k82, I
     {0, CS7_SW10,  CS8_SW10,  CS9_SW10},  //  45, k92, O
     {0, CS7_SW11,  CS8_SW11,  CS9_SW11},  //  46, ka2, P
     {0, CS7_SW12,  CS8_SW12,  CS9_SW12},  //  47, kb2, [
@@ -82,13 +113,13 @@ const aw_led g_aw_leds[DRIVER_LED_TOTAL] = {
     {1, CS7_SW7,   CS8_SW7,   CS9_SW7},   //  52, ka6, Num 9
     {1, CS7_SW8,   CS8_SW8,   CS9_SW8},   //  53, kb6, Num +
 
-    {0, CS10_SW1,  CS11_SW1,  CS12_SW1},  //*  54, k03, Caps Lock
-    {0, CS10_SW2,  CS11_SW2,  CS12_SW2},  //*  55, k13, A
-    {0, CS10_SW3,  CS11_SW3,  CS12_SW3},  //*  56, k23, S
-    {0, CS10_SW4,  CS11_SW4,  CS12_SW4},  //*  57, k33, D
-    {0, CS10_SW5,  CS11_SW5,  CS12_SW5},  //*  58, k43, F
-    {0, CS10_SW6,  CS11_SW6,  CS12_SW6},  //*  59, k53, G
-    {0, CS10_SW7,  CS11_SW7,  CS12_SW7},  //*  60, k63, H
+    //{0, CS10_SW1,  CS11_SW1,  CS12_SW1},  //*  54, k03, Caps Lock
+    //{0, CS10_SW2,  CS11_SW2,  CS12_SW2},  //*  55, k13, A
+    //{0, CS10_SW3,  CS11_SW3,  CS12_SW3},  //*  56, k23, S
+    //{0, CS10_SW4,  CS11_SW4,  CS12_SW4},  //*  57, k33, D
+    //{0, CS10_SW5,  CS11_SW5,  CS12_SW5},  //*  58, k43, F
+    //{0, CS10_SW6,  CS11_SW6,  CS12_SW6},  //*  59, k53, G
+    //{0, CS10_SW7,  CS11_SW7,  CS12_SW7},  //*  60, k63, H
     {0, CS10_SW8,  CS11_SW8,  CS12_SW8},  //  61, k73, J
     {0, CS10_SW9,  CS11_SW9,  CS12_SW9},  //  62, k83, K
     {0, CS10_SW10, CS11_SW10, CS12_SW10}, //  63, k93, L
